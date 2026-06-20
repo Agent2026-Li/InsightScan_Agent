6b3df9beac617a4d799e349ae953d49172d34d92
Agent2006 <2839577861@qq.com>
2026-06-20 12:03:51 +0800

    Delete InsightScan智扫通 directory

--- InsightScan智扫通/02-ZST_code/Tests/extract_info_test.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-# 添加项目根目录到 sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Tools.ExtractInfo import extract_info
-
-print(extract_info("根据我的扫地机器人的使用情况，生成一份使用报告,要2025年6月份的"))
--- InsightScan智扫通/02-ZST_code/Tests/rag_test.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-# 添加项目根目录到 sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Tools.RagQATool import rag_qa
-
-print(rag_qa("为什么有些机器人会'迷路'？"))
